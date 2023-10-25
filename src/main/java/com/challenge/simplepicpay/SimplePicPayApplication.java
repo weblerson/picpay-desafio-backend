81656e72b671149a40f5de8b5b162248913c7a3c
weblerson <zonejr4@gmail.com>
2023-10-25 11:54:55 -0400

    first commit

---
+++ src/main/java/com/challenge/simplepicpay/SimplePicPayApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.simplepicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePicPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/simplepicpay/SimplePicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.simplepicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
