7775c3d396c394c0e335a7e78e43f7b0ed0ef8a8
weblerson <zonejr4@gmail.com>
2023-10-25 11:55:28 -0400

    feat: created Transaction entity

---
+++ src/main/java/com/challenge/simplepicpay/entities/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package com.challenge.simplepicpay.entities.transaction;
+
+import com.challenge.simplepicpay.entities.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    public Transaction() {}
+}
