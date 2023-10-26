33e17333a8c01361d95b55d3471e0a76607b06b6
weblerson <zonejr4@gmail.com>
2023-10-26 14:44:21 -0400

    refactor: <Transaction.java> implemented NoArgsConstructor annotation

--- src/main/java/com/challenge/simplepicpay/entities/transaction/Transaction.java
+++ src/main/java/com/challenge/simplepicpay/entities/transaction/Transaction.java
@@ -2,10 +2,7 @@ package com.challenge.simplepicpay.entities.transaction;
 
 import com.challenge.simplepicpay.entities.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class Transaction {
 
@@ -27,6 +25,4 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "receiver_id")
     private User receiver;
-
-    public Transaction() {}
 }
