ae1cb0c07a4f1f865aaaee17ec8074ca43665bd4
weblerson <zonejr4@gmail.com>
2023-10-26 15:14:52 -0400

    refactor: UserResponseDTO is now a record class

--- src/main/java/com/challenge/simplepicpay/dto/user/UserResponseDTO.java
+++ src/main/java/com/challenge/simplepicpay/dto/user/UserResponseDTO.java
@@ -2,27 +2,11 @@ package com.challenge.simplepicpay.dto.user;
 
 import com.challenge.simplepicpay.entities.user.User;
 import com.challenge.simplepicpay.entities.user.UserType;
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-public class UserResponseDTO {
-
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private UserType userType;
+public record UserResponseDTO(Long id, String firstName, String lastName, String email, UserType userType) {
 
     public UserResponseDTO(User user) {
 
-        this.id = user.getId();
-        this.firstName = user.getFirstName();
-        this.lastName = user.getLastName();
-        this.email = user.getEmail();
-        this.userType = user.getUserType();
+        this(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getUserType());
     }
 }
