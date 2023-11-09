02ea7f3bbfe933bd2e866c47aeb03e1462d49ba7
weblerson <zonejr4@gmail.com>
2023-11-09 13:16:56 -0400

    docs: created api definition

--- src/main/java/com/challenge/simplepicpay/SimplePicPayApplication.java
+++ src/main/java/com/challenge/simplepicpay/SimplePicPayApplication.java
@@ -1,9 +1,12 @@
 package com.challenge.simplepicpay;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "SimplePicPay API", version = "1.0", description = "SimplePicPay API"))
 public class SimplePicPayApplication {
 
 	public static void main(String[] args) {
